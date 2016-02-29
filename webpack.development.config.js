fcf2915eeaf9fd75d67ce316b1948c4ce5492483
Matt Casey <matt_casey@mckinsey.com>
2016-02-28 19:49:00 -0500

    basically working in dev mode

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,34 +1,34 @@
-var path = require('path');
-var webpack = require('webpack');
-var config = {
+const path = require('path');
+const webpack = require('webpack');
+const config = {
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   entry: [
     'webpack/hot/dev-server',
     'webpack-dev-server/client?http://localhost:8080',
-    path.resolve(__dirname, 'app/index.js')
+    path.resolve(__dirname, 'app/index.js'),
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [{
       test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
       exclude: /node_modules/,
-      loaders: ['react-hot', 'babel']
+      loaders: ['react-hot', 'babel'],
     }, {
       test: /\.scss$/,
-      loaders: ['style', 'css', 'sass']
+      loaders: ['style', 'css', 'sass'],
     }, {
       test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      loader: 'url-loader?limit=10000&mimetype=application/font-woff',
     }, {
       test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'file-loader'
-    }]
-  }
+      loader: 'file-loader',
+    }],
+  },
 };
 
 module.exports = config;
