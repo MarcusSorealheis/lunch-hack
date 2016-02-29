12af9a7ad9dca47e3fd1ee408c3ffe95b788a31d
Matt Casey <matt_casey@mckinsey.com>
2016-02-28 21:33:13 -0500

    button types

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './_Button.scss';
 import { browserHistory } from 'react-router';
+import classNames from 'classnames';
 
 export default class Home extends React.Component {
   static propTypes = {
     text: React.PropTypes.string.isRequired,
     linkTo: React.PropTypes.string,
     handleClick: React.PropTypes.func,
-  }
+    type: React.PropTypes.string,
+  };
+
+  static defaultProps = {
+    type: 'primary',
+  };
 
   _handleClick = () => {
     if (this.props.linkTo) browserHistory.push(this.props.linkTo);
@@ -15,8 +21,16 @@ export default class Home extends React.Component {
   }
 
   render() {
+    const buttonClasses = classNames(
+      'button',
+      {
+        'button--primary': this.props.type === 'primary',
+        'button--secondary': this.props.type === 'secondary',
+      },
+    );
+
     return (
-      <button className="button button--primary" onClick={this._handleClick}>
+      <button className={buttonClasses} onClick={this._handleClick}>
         { this.props.text }
       </button>
     );
