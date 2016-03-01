cb6eb14efb8c8f6c9fbf618a22254ad53fda643a
Matt Casey <matt_casey@mckinsey.com>
2016-03-01 13:48:24 -0500

    t and c

--- app/components/Intro.js
+++ app/components/Intro.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Button from './Button';
+import TermsAndConditions from './TermsAndConditions';
 import './_Intro.scss';
 
 export default class Intro extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showTandC: false,
+    };
+  }
+
   render() {
     return (
       <div className="intro-page">
+        <TermsAndConditions show={this.state.showTandC}/>
         <div className="row row--centered">
           <div className="col col--2-3">
             <span className="mega u-strong u-uppercase">
@@ -13,7 +22,7 @@ export default class Intro extends React.Component {
               free and reduced price meals <br />
               <br />
             </span>
-            <Button className="u-center" linkTo="/applicant/step-1" text="APPLY (5-10 MINS)" />
+            <Button className="u-center" text="APPLY (5-10 MINS)" onClick={() => this.setState({ showTandC: true })} />
           </div>
         </div>
       </div>
